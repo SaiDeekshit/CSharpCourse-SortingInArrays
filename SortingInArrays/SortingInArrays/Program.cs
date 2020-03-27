921ef726cc231dde5417bde98701dccd06defafb
Sai Deekshith <msaideekshith@gmail.com>
2020-03-27 07:00:41 +0530

    sorting in arrays

---
+++ SortingInArrays/SortingInArrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SortingInArrays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 2, 1, 4, 3 };
+            String[] numberNames = { "two", "one", "four", "three" };
+
+            Array.Sort(numbers, numberNames);
+
+            Array.ForEach<int>(numbers, n => Console.WriteLine(n));//[1,2,3,4]
+            Array.ForEach<string>(numberNames, s => Console.WriteLine(s));//["one", "two", "three", "four"]
+        }
+    }
+}
